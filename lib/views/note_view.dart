655f32bd115c07e48a34906421fb6e00d56478d6
abdoo <abdoalgamal84@gmail.com>
2024-03-05 08:38:07 +0200

    add border radius to note sheet bottom

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -12,6 +12,7 @@ class NoteView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+            shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
                 return const NoteBottomSheet();
